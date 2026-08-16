eafaf11a4f6c1409d6056e36b1e4beea921556e0
d41bf92 <313914634+d41bf92@users.noreply.github.com>
2026-08-16 10:23:37 +0800

    first commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.3
+
+import PackageDescription
+
+let package = Package(
+    name: "CodeReset",
+    platforms: [
+        .macOS(.v26)
+    ],
+    products: [
+        .executable(name: "code-reset", targets: ["CodeReset"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.8.2")
+    ],
+    targets: [
+        .executableTarget(
+            name: "CodeReset",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")])
+    ],
+    swiftLanguageModes: [.v6]
+)
